be8b11dbd6f6bae1552808e8759a3c43e1b01528
Diego <diegoalexandrooliveira@gmail.com>
2017-09-18 14:25:12 -0300

    a

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,6 +1,12 @@
 var titulo = document.querySelector('.titulo');
 titulo.textContent = 'Aparecida nutricionista';
 
+titulo.addEventListener("click", mostraMensagem);
+
+function mostraMensagem() {
+  console.log("Clicou");
+};
+
 var pacientes = document.querySelectorAll(".paciente");
 
 for (var i = 0; i < pacientes.length; i++) {
@@ -19,16 +25,22 @@ for (var i = 0; i < pacientes.length; i++) {
     pesoValido = false;
     console.log("Peso inválido!");
     tdIMC.textContent = "Peso inválido!";
+    paciente.classList.add("paciente-invalido");
   }
   if (altura <= 0 || altura >= 3) {
     alturaValida = false;
     console.log("Altura inválida!");
     tdIMC.textContent = "Altura inválido!";
+    paciente.classList.add("paciente-invalido");
   }
 
   if (alturaValida && pesoValido) {
     var imc = peso / (altura * altura);
     tdIMC.textContent = imc.toFixed(2);
-    console.log("");
   }
 }
+
+var botaoAdicionar = document.querySelector('#adicionar-paciente');
+botaoAdicionar.addEventListener("click", function() {
+  console.log("Clicou");
+});
