60e9d5376e00423c8af8d61f704fde83ded935f8
Diego <diegoalexandrooliveira@gmail.com>
2017-09-29 20:00:43 -0300

    .

--- javascript-avancado-i-aula1/aluraframe/client/js/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/controllers/NegociacaoController.js
@@ -6,12 +6,14 @@ class NegociacaoController {
     this._inputQuantidade = this.$("#quantidade");
     this._inputValor = this.$("#valor");
 
-    this._listaNegociacoes = new ListaNegociacoes(function(model) {
-      this._negociacoesView.update(model);
-    }.bind(this));
+
+    this._listaNegociacoes = new ListaNegociacoes();
     this._negociacoesView = new NegociacoesView(this.$("#negociacoesView"));
     this._negociacoesView.update(this._listaNegociacoes);
 
+    // this._listaNegociacoes = new ListaNegociacoes(function(model) {
+    //   this._negociacoesView.update(model);
+    // }.bind(this));
     // estou usando bind, mas dá pra usar o Reflect.apply, ele pede a função que quero executar
     // e qual o contexto que essa função deve ser executada
     //  Dá pra usar a arrow function, o this da => não é dinâmico
