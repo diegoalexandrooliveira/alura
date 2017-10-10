2f127b40039714f5a025f5e0aa1ac1d868b098b2
Diego <diegoalexandrooliveira@gmail.com>
2017-10-10 13:42:51 -0300

    .

--- javascript-avancado-i-aula1/aluraframe/client/js/services/HttpService.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/services/HttpService.js
@@ -33,12 +33,32 @@ class HttpService {
 
     return new Promise((resolve, reject) => {
 
-        let xhr = new XMLHttpRequest();
-        xhr.open("POST", url, true);
-        xhr.setRequestHeader("Content-Type", "application/json");
+      let xhr = new XMLHttpRequest();
+      xhr.open("POST", url, true);
+      xhr.setRequestHeader("Content-Type", "application/json");
+
+      xhr.onreadystatechange = () => {
+        /*
+        Estados de uma requisição
+        0: requisição ainda não iniciada
+        1: conexão com o servidor estabelecida
+        2: requisição recebida
+        3: processando requisição
+        4: requisição concluída e a resposta está pronta
+        */
+        if (xhr.readyState == 4) {
+          if (xhr.status == 200) {
+            resolve(JSON.parse(xhr.responseText));
+          } else {
+            reject(xhr.responseText);
+          }
+        }
+      }
+
+      xhr.send(JSON.stringify(dado));
 
     });
 
-}
+  }
 
 }
