caa52b41e46ef8922a6358e9f4d9b1970a3c27b3
Diego <diegoalexandrooliveira@gmail.com>
2017-10-13 20:21:29 -0300

    .

---
+++ javascript-avancado-i-aula1/aluraframe/client/js/services/ConnectionFactory.js
@@ -0,0 +1,47 @@
+var ConnectionFactory = (function() {
+  var stores = ['negociacoes'];
+  var version = 4;
+  var dbName = 'aluraframe';
+
+  var connection = null;
+
+  return class ConnectionFactory {
+
+    constructor() {
+      throw new Error('Classe estática');
+    }
+
+    static getConnection() {
+      return new Promise((resolve, reject) => {
+        let openRequest = window.indexedDB.open(dbName, version);
+
+        openRequest.onupgradeneeded = e => {
+          ConnectionFactory._createStores(e.target.result);
+        };
+
+        openRequest.onsuccess = e => {
+          if (!connection) connection = e.target.result;
+          resolve(connection);
+        };
+
+        openRequest.onerror = e => {
+          console.log(e.target.error);
+          reject(e.target.error.name);
+        };
+
+
+      });
+    }
+
+    static _createStores(connection) {
+      stores.forEach(store => {
+        if (connection.objectStoreNames.contains(store))
+          connection.deleteObjectStore(store);
+
+        connection.createObjectStore(store, {
+          autoIncrement: true
+        });
+      });
+    }
+  }
+})();
