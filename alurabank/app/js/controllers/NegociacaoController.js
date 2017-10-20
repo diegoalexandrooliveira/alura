f565bb5d282d475e6740da5fad94e6c188774e5a
Diego <diegoalexandrooliveira@gmail.com>
2017-10-20 20:27:10 -0200

    .

---
+++ alurabank/app/js/controllers/NegociacaoController.js
@@ -0,0 +1,12 @@
+class NegociacaoController {
+    constructor() {
+        this._inputData = document.querySelector("#data");
+        this._inputQuantidade = document.querySelector("#quantidade");
+        this._inputValor = document.querySelector("#valor");
+    }
+    adiciona(event) {
+        event.preventDefault();
+        const negociacao = new Negociacao(new Date(this._inputData.value), Number(this._inputQuantidade.value), Number(this._inputValor.value));
+        console.log(negociacao);
+    }
+}
