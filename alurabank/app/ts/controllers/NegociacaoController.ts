e79adcc77226acc676d0ac53dec9aab94dd081ba
Diego <diegoalexandrooliveira@gmail.com>
2017-10-23 14:21:28 -0200

    .

--- alurabank/app/ts/controllers/NegociacaoController.ts
+++ alurabank/app/ts/controllers/NegociacaoController.ts
@@ -3,20 +3,27 @@ class NegociacaoController {
   private _inputData: HTMLInputElement;
   private _inputQuantidade: HTMLInputElement;
   private _inputValor: HTMLInputElement;
+  private _negociacoes = new Negociacoes();
+  private _negociacoesView = new NegociacoesView('#negociacoesView');
 
   constructor() {
     this._inputData = <HTMLInputElement>document.querySelector("#data");
     this._inputQuantidade = <HTMLInputElement>document.querySelector("#quantidade");
     this._inputValor = <HTMLInputElement>document.querySelector("#valor");
+    this._negociacoesView.update(this._negociacoes);
   }
 
   adiciona(event: Event) {
     event.preventDefault();
     const negociacao = new Negociacao(
-      new Date(this._inputData.value),
+      new Date(this._inputData.value.replace(/-/g, ',')),
       Number(this._inputQuantidade.value),
       Number(this._inputValor.value)
     );
-    console.log(negociacao);
+    this._negociacoes.adiciona(negociacao);
+    this._negociacoesView.update(this._negociacoes);
+
+    this._negociacoes.paraArray()
+      .forEach(negociacao => console.log(negociacao));
   }
 }
---
+++ alurabank/app/ts/models/Negociacoes.ts
@@ -0,0 +1,13 @@
+class Negociacoes {
+  private _negociacoes: Negociacao[] = [];
+  // private _negociacoes: Array<Negociacao> = [];
+
+  public adiciona(negociacao: Negociacao): void {
+    this._negociacoes.push(negociacao);
+  }
+
+  paraArray(): Negociacao[] {
+    return [].concat(this._negociacoes);
+  }
+
+}
