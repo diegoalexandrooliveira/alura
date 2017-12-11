772658f923d2f9ce5edef4a854ca1f014600e89d
Diego <diegoalexandrooliveira@gmail.com>
2017-12-11 20:08:14 -0200

    .

--- gulp/projeto/gulpfile.js
+++ gulp/projeto/gulpfile.js
@@ -1,9 +1,37 @@
 var gulp = require("gulp"),
-  gulp_imgagemin = require("gulp-imagemin");
+  gulp_imgagemin = require("gulp-imagemin"),
+  clean = require('gulp-clean'),
+  concat = require('gulp-concat'),
+  htmlReplace = require('gulp-html-replace');
 
-gulp.task("build-img", function() {
-  gulp
-    .src("src/img/**/*")
+gulp.task('default', ['copy'], function () {
+  gulp.start('build-img', 'build-js', 'build-html');
+});
+
+gulp.task('clean', function () {
+  return gulp.src('dist').pipe(clean());
+});
+
+gulp.task('copy', ['clean'], function () {
+  return gulp.src('src/**/*').pipe(gulp.dest('dist'));
+});
+
+gulp.task("build-img", function () {
+  gulp.src("dist/img/**/*")
     .pipe(gulp_imgagemin())
-    .pipe(gulp.dest("src/img"));
+    .pipe(gulp.dest("dist/img"));
+});
+
+gulp.task("build-js", function () {
+  gulp.src(['dist/js/jquery.js', 'dist/js/home.js', 'dist/js/produto.js'])
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('build-html', function () {
+  gulp.src('dist/**/*.html')
+    .pipe(htmlReplace({
+      js: 'js/all.js'
+    }))
+    .pipe(gulp.dest('dist'));
 });
