45ad1a80aba9bddc4fedaa83b26ee83e852dd804
Diego <diegoalexandrooliveira@gmail.com>
2017-12-13 14:14:04 -0200

    .

--- gulp/projeto/gulpfile.js
+++ gulp/projeto/gulpfile.js
@@ -1,37 +1,58 @@
 var gulp = require("gulp"),
   gulp_imgagemin = require("gulp-imagemin"),
-  clean = require('gulp-clean'),
-  concat = require('gulp-concat'),
-  htmlReplace = require('gulp-html-replace');
+  clean = require("gulp-clean"),
+  // concat = require("gulp-concat"),
+  // htmlReplace = require("gulp-html-replace"),
+  uglify = require("gulp-uglify"),
+  usemin = require("gulp-usemin"),
+  cssmin = require("gulp-cssmin");
 
-gulp.task('default', ['copy'], function () {
-  gulp.start('build-img', 'build-js', 'build-html');
+gulp.task("default", ["copy"], function() {
+  gulp.start("build-img", "usemin");
 });
 
-gulp.task('clean', function () {
-  return gulp.src('dist').pipe(clean());
+gulp.task("clean", function() {
+  return gulp.src("dist").pipe(clean());
 });
 
-gulp.task('copy', ['clean'], function () {
-  return gulp.src('src/**/*').pipe(gulp.dest('dist'));
+gulp.task("copy", ["clean"], function() {
+  return gulp.src("src/**/*").pipe(gulp.dest("dist"));
 });
 
-gulp.task("build-img", function () {
-  gulp.src("dist/img/**/*")
+gulp.task("build-img", function() {
+  gulp
+    .src("dist/img/**/*")
     .pipe(gulp_imgagemin())
     .pipe(gulp.dest("dist/img"));
 });
 
-gulp.task("build-js", function () {
-  gulp.src(['dist/js/jquery.js', 'dist/js/home.js', 'dist/js/produto.js'])
-    .pipe(concat('all.js'))
-    .pipe(gulp.dest('dist/js'));
-});
+// gulp.task("build-js", function() {
+//   gulp
+//     .src(["dist/js/jquery.js", "dist/js/home.js", "dist/js/produto.js"])
+//     .pipe(concat("all.js"))
+//     .pipe(uglify())
+//     .pipe(gulp.dest("dist/js"));
+// });
 
-gulp.task('build-html', function () {
-  gulp.src('dist/**/*.html')
-    .pipe(htmlReplace({
-      js: 'js/all.js'
-    }))
-    .pipe(gulp.dest('dist'));
+// gulp.task("build-html", function() {
+//   gulp
+//     .src("dist/**/*.html")
+//     .pipe(
+//       htmlReplace({
+//         js: "js/all.js"
+//       })
+//     )
+//     .pipe(gulp.dest("dist"));
+// });
+
+gulp.task("usemin", function() {
+  gulp
+    .src("dist/**/*")
+    .pipe(
+      usemin({
+        js: [uglify],
+        css: [cssmin]
+      })
+    )
+    .pipe(gulp.dest("dist"));
 });
