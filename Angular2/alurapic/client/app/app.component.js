6b51d9b2933729b3054bf4ee628329700f6102db
Diego <diegoalexandrooliveira@gmail.com>
2018-01-05 14:14:53 -0200

    .

--- Angular2/alurapic/client/app/app.component.js
+++ Angular2/alurapic/client/app/app.component.js
@@ -5,10 +5,20 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var http_1 = require("@angular/http");
 var AppComponent = (function () {
-    function AppComponent() {
+    function AppComponent(http) {
+        var _this = this;
+        this.fotos = [];
+        http.get("http://localhost:3001/v1/fotos").subscribe(function (res) {
+            _this.fotos = res.json();
+            console.log(_this.fotos);
+        });
     }
     return AppComponent;
 }());
@@ -17,7 +27,8 @@ AppComponent = __decorate([
         moduleId: module.id,
         selector: "app",
         templateUrl: "./app.component.html"
-    })
+    }),
+    __metadata("design:paramtypes", [http_1.Http])
 ], AppComponent);
 exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
