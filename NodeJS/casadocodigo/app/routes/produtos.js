e890d24324757c7a952c31ad791d748eff95ff65
Diego <diegoalexandrooliveira@gmail.com>
2018-01-18 14:01:47 -0200

    .

--- NodeJS/casadocodigo/app/routes/produtos.js
+++ NodeJS/casadocodigo/app/routes/produtos.js
@@ -1,26 +1,35 @@
 module.exports = app => {
-    app.get("/produtos", (req, res) => {
-        let conexao = app.infra.connectionFactory();
-        let produtosDAO = new app.infra.ProdutosDAO(conexao);
-        produtosDAO.lista((erro, resultados) => {
-            res.render("produtos/lista", {
-                lista: resultados
-            });
-        });
-        conexao.end();
+  app.get("/produtos", (req, res) => {
+    let conexao = app.infra.connectionFactory();
+    let produtosDAO = new app.infra.ProdutosDAO(conexao);
+    produtosDAO.lista((erro, resultados) => {
+      res.render("produtos/lista", {
+        lista: resultados
+      });
     });
+    conexao.end();
+  });
 
-    app.get("/produtos/novo", (req, res) => {
-        res.render("produtos/form");
-    });
+  app.get("/produtos/novo", (req, res) => {
+    res.render("produtos/form");
+  });
 
-    app.post("/produtos/salvar", (req, res) => {
-        let produto = req.body;
-        let conexao = app.infra.connectionFactory();
-        let produtosDAO = new app.infra.ProdutosDAO(conexao);
-        produtosDAO.inserir(produto, (erro, resultados) => {
-            res.redirect('/produtos');
-        });
-        conexao.end();
+  app.post("/produtos", (req, res) => {
+    let produto = req.body;
+    let conexao = app.infra.connectionFactory();
+    let produtosDAO = new app.infra.ProdutosDAO(conexao);
+    produtosDAO.inserir(produto, erro => {
+      res.redirect("/produtos");
     });
+    conexao.end();
+  });
+
+  app.delete("/produtos/:id", (req, res) => {
+    let conexao = app.infra.connectionFactory();
+    let produtosDAO = new app.infra.ProdutosDAO(conexao);
+    produtosDAO.deletar(req.params.id, erro => {
+      res.redirect("/produtos");
+    });
+    conexao.end();
+  });
 };
