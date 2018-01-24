fb33a20b4a672aa464ac7ff0bce93301103b32fe
Diego <diegoalexandrooliveira@gmail.com>
2018-01-24 13:51:35 -0200

    .

--- NodeJS/casadocodigo/app/infra/connectionFactory.js
+++ NodeJS/casadocodigo/app/infra/connectionFactory.js
@@ -1,14 +1,13 @@
 let mysql = require("mysql");
 
 function createDBConnection() {
-
   if (!process.env.NODE_ENV) {
     return mysql.createConnection({
       host: "localhost",
       port: 3306,
       user: "root",
       password: "root",
-      database: "casadocodigo_nodej1s"
+      database: "casadocodigo_nodejs"
     });
   } else if (process.env.NODE_ENV == "test") {
     return mysql.createConnection({
@@ -18,6 +17,23 @@ function createDBConnection() {
       password: "root",
       database: "casadocodigo_nodejs_test"
     });
+  } else if (process.env.NODE_ENV == "production") {
+    // posso informar assim ouuuuu acessar variavel de ambiente
+    return mysql.createConnection({
+      host: "us-cdbr-iron-east-05.cleardb.net",
+      user: "b1d0ff4f170764",
+      password: "27ab6d6e",
+      database: "heroku_2ae4a1333768240"
+    });
+    // let infoConexao = process.env.CLEARDB_DATABASE_URL.match(
+    //   /mysql:\/\/(.*):(.*)@(.*)\/(.*)\?reconnect=true/
+    // );
+    // return mysql.createConnection({
+    //   host: infoConexao[3],
+    //   user: "b1d0ff4f170764",
+    //   password: "27ab6d6e",
+    //   database: "heroku_2ae4a1333768240"
+    // });
   }
 }
 
