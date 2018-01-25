892dca0da351a73f0931325f58bfcf684ef82c5d
Diego <diegoalexandrooliveira@gmail.com>
2018-01-25 13:26:21 -0200

    .

--- NodeJS/casadocodigo/app.js
+++ NodeJS/casadocodigo/app.js
@@ -2,10 +2,10 @@ let app = require("./config/express")();
 let http = require("http").Server(app);
 let socket = require("socket.io")(http);
 
-const porta = 3000;
+let porta = process.env.PORT || 3000;
 
 app.set("io", socket);
 
 http.listen(porta, () => {
-    console.log("Rodando na porta " + porta);
+  console.log("Rodando na porta " + porta);
 });
