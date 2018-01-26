ebebdf7049c5d76302368bf5b9595c45c079184a
Diego <diegoalexandrooliveira@gmail.com>
2018-01-26 20:08:10 -0200

    .

---
+++ NodeJS_REST/payfast/config/custom-express.js
@@ -0,0 +1,11 @@
+let express = require("express");
+let consign = require("consign");
+let bodyParser = require("body-parser");
+
+module.exports = () => {
+    let app = express();
+    app.use(bodyParser.json());
+    consign().include("controllers")
+        .into(app);
+    return app;
+};
---
+++ NodeJS_REST/payfast/controllers/pagamentos.js
@@ -0,0 +1,15 @@
+module.exports = (app) => {
+    app.get("/pagamentos", (request, response) => {
+        console.log("Funcionou");
+        response.send("Funcionou");
+    });
+
+
+    app.post("/pagamentos/pagamento", (request, response) => {
+        let pagamento = request.body;
+        console.log(pagamento);
+        pagamento.status = "CRIADO";
+        pagamento.data = new Date();
+        response.send(pagamento);
+    });
+}
